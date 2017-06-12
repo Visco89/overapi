fb09d77345250e99ac69876b9ad8f48c208692b7
Marco Visconti <visco.marco@gmail.com>
2017-06-12 23:28:37 +0200

    career

--- sample/app.py
+++ sample/app.py
@@ -5,39 +5,43 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Welcome to overapi!"
 
+
 @app.route('/overapi/v1.0/<name>/<id>/generic_stats', methods=['GET'])
 def get_generic_stats(name, id):
-    user = name + '-' +id
+    user = name + '-' + id
     resp = {}
     try:
         resp['result'] = core.generic_stats(user);
     except requests.exceptions.HTTPError as err:
-        return str({"status" : "KO", "error" : str(err)})
+        return str({"status": "KO", "error": str(err)})
     resp["status"] = "OK";
     return str(resp)
 
+
 @app.route('/overapi/v1.0/<name>/<id>/featured_stat', methods=['GET'])
 def get_featured_stat(name, id):
-    user = name + '-' +id
+    user = name + '-' + id
     mode = request.args.get('mode')
     resp = {}
     if mode != None and mode != "competitive" and mode != "quickplay":
-        return str({"status" : "KO", "error" : "incorrect mode"})
+        return str({"status": "KO", "error": "incorrect mode"})
     try:
         resp['result'] = core.featured_stat(user, mode)
     except requests.exceptions.HTTPError as err:
-        return str({"status" : "KO", "error" : str(err)})
-    if mode=="competitive" and (resp == None or resp == {}):
+        return str({"status": "KO", "error": str(err)})
+    if mode == "competitive" and (resp == None or resp == {}):
         return str({"status": "KO", "error": "no competitive data"})
     resp["status"] = "OK";
     return str(resp)
 
+
 @app.route('/overapi/v1.0/<name>/<id>/top_heroes', methods=['GET'])
-def get_top_heroes(name,id):
+def get_top_heroes(name, id):
     user = name + '-' + id
     mode = request.args.get('mode')
     category = request.args.get('category')
@@ -47,9 +51,27 @@ def get_top_heroes(name,id):
     try:
         resp['result'] = core.top_heroes(user, category, mode)
     except requests.exceptions.HTTPError as err:
-        return str({"status" : "KO", "error" : str(err)})
-    except KeyError as err2:
+        return str({"status": "KO", "error": str(err)})
+    except KeyError:
         return str({"status": "KO", "error": "incorrect category"})
+    resp["status"] = "OK";
+    return str(resp)
+
+
+@app.route('/overapi/v1.0/<name>/<id>/career', methods=['GET'])
+def get_career(name, id):
+    user = name + '-' + id
+    mode = request.args.get('mode')
+    champ = request.args.get('champ')
+    resp = {}
+    if mode != None and mode != "competitive" and mode != "quickplay":
+        return str({"status": "KO", "error": "incorrect mode"})
+    try:
+        resp['result'] = core.career(user, champ, mode)
+    except requests.exceptions.HTTPError as err:
+        return str({"status": "KO", "error": str(err)})
+    except KeyError:
+        return str({"status": "KO", "error": "incorrect champion"})
     resp["status"] = "OK";
     return str(resp)
 
--- sample/core.py
+++ sample/core.py
@@ -2,14 +2,16 @@ import util
 
 from div_costant import CHAMPION, TOP_HEROES_CATEGORY
 
+
 def generic_stats(user):
     tree = util.base_tree(user);
     rank = tree.xpath('//div[@class="competitive-rank"]/div')[0].text
     level = tree.xpath('//div[@class="player-level"]/div')[0].text
     return {
-        "rank" : rank,
-        "level" : level
+        "rank": rank,
+        "level": level
     }
+
 
 def featured_stat(user, _par=None):
     print(_par)
@@ -23,6 +25,7 @@ def featured_stat(user, _par=None):
     keys = tree.xpath('//div[@id="{}"]//p[@class="card-copy"]/text()'.format(mode))
     return dict(zip(keys, values))
 
+
 def top_heroes(user, _cat=None, _mode=None):
     if _cat is None:
         category = TOP_HEROES_CATEGORY["time_played"]
@@ -33,19 +36,28 @@ def top_heroes(user, _cat=None, _mode=None):
     else:
         mode = _mode
     tree = util.base_tree(user);
-    keys = tree.xpath('//div[@id="{}"]//div[@data-category-id="{}"]/div/div/div/div[@class="title"]/text()'.format(mode,category))
-    values = tree.xpath('//div[@id="{}"]//div[@data-category-id="{}"]/div/div/div/div[@class="description"]/text()'.format(mode,category))
+    keys = tree.xpath(
+        '//div[@id="{}"]//div[@data-category-id="{}"]/div/div/div/div[@class="title"]/text()'.format(mode, category))
+    values = tree.xpath(
+        '//div[@id="{}"]//div[@data-category-id="{}"]/div/div/div/div[@class="description"]/text()'.format(mode,
+                                                                                                           category))
     return dict(zip(keys, values))
 
-def carreer_stats(user, champ = CHAMPION["all"], mode="quickplay"):
+
+def career(user, _champ=None, _mode=None):
+    if _champ is None:
+        champ = CHAMPION["all"]
+    else:
+        champ = CHAMPION[_champ]
+    if _mode is None:
+        mode = "quickplay"
+    else:
+        mode = _mode
     tree = util.base_tree(user);
-    keys = tree.xpath('//div[@id="{}"]//div[@data-category-id="{}"]//tbody/tr/td[1]/text()'.format(mode,champ))
-    values = tree.xpath('//div[@id="{}"]//div[@data-category-id="{}"]//tbody/tr/td[2]/text()'.format(mode,champ))
+    keys = tree.xpath('//div[@id="{}"]//div[@data-category-id="{}"]//tbody/tr/td[1]/text()'.format(mode, champ))
+    values = tree.xpath('//div[@id="{}"]//div[@data-category-id="{}"]//tbody/tr/td[2]/text()'.format(mode, champ))
     return dict(zip(keys, values))
+
 
 if __name__ == '__main__':
     exit()
-# print(generic_stats())
-# print(featured_stat('Aren-2596'))
-# print(top_heroes(top_heroes_category["time_played"]))
-# print(carreer_stats(champion["reaper"]))
